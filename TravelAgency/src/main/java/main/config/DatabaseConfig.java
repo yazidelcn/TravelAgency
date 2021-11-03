737593cf1fc3ec48c4ce6f8391a2ca29db9a6e4e
Yazid Elcouhen <elcouhenyazid@gmail.com>
2021-11-03 02:35:34 +0100

    configuring hibernate and the database

---
+++ TravelAgency/src/main/java/main/config/DatabaseConfig.java
@@ -0,0 +1,53 @@
+package main.config;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("main")
+@PropertySource("WEB-INF/resources/database.properties")
+public class DatabaseConfig {
+	@Autowired
+	private Environment environment;
+	
+	@Bean
+	public DataSource getDataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName(environment.getProperty("jdbc.driver"));
+		dataSource.setUrl(environment.getProperty("jdbc.url"));
+		dataSource.setUsername(environment.getProperty("jdbc.username"));
+		dataSource.setPassword(environment.getProperty("jdbc.password"));
+		return dataSource;
+	}
+	
+	@Bean
+	public LocalSessionFactoryBean sessionFactoryBean() {
+		LocalSessionFactoryBean localSessionFactoryBean = new LocalSessionFactoryBean();
+		localSessionFactoryBean.setDataSource(getDataSource());
+		localSessionFactoryBean.setPackagesToScan(new String[] {"main"});
+		localSessionFactoryBean.setHibernateProperties(hibernateProperties());
+		return localSessionFactoryBean;
+	}
+	
+	private final Properties hibernateProperties() {
+		Properties properties = new Properties();
+		properties.put("hibernate.show_sql", environment.getProperty("hibernate.show_sql"));
+		properties.put("hibernate.dialect", environment.getProperty("hibernate.dialect"));
+		properties.put("hibernate.hbm2ddl.auto", environment.getProperty("hibernate.hbm2ddl.auto"));
+		return properties;
+	}
+	
+
+}
